a06829e07d058317b951aafda57e1f856f585c23
drzaiusx11 <drzaiusx11@gmail.com>
2016-01-16 16:51:02 +0000

    add ability to compile on raspberry pi

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "WiringVCS.h"
+
+int main(void) {
+	uint8_t rom[4096];
+
+	FILE* file = fopen("rom.bin", "wb");
+
+	VCS.setup();
+
+	Cart cart = VCS.findCart();
+
+	VCS.dump(cart, rom, 0, cart.size);
+
+	fwrite(rom, 1, cart.size, file);
+
+	fclose(file);
+
+	return 0;
+}
+
