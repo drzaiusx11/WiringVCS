985a69bb0781eab0b9e717a99a5ccb2ff168c725
drzaiusx11 <drzaiusx11@gmail.com>
2016-01-16 12:37:12 -0500

    add arduino demo

---
+++ src/demos/arduino/dumper.ino
@@ -0,0 +1,20 @@
+#include <WiringVCS.h>
+
+#define NBYTES 32
+
+void setup() {
+  VCS.setup();
+  
+  Cart cart = VCS.findCart();
+
+  uint8_t partialRom[NBYTES];
+
+  for (int offset = 0; offset < cart.size; offset += NBYTES) {
+    VCS.dump(cart, partialRom, offset, NBYTES);
+    Serial.write(partialRom, NBYTES);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
